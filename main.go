42551878ad5eaf548fa2e5a2085f97632981cd31
Ken Minami <kenminami.dev@gmail.com>
2020-08-05 08:35:28 +0900

    non-struct

--- main.go
+++ main.go
@@ -2,48 +2,15 @@ package main
 
 import "fmt"
 
-// Vertex struct
-type Vertex struct {
-	X, Y int
-}
+// MyInt : struct
+type MyInt int
 
-// Area func : value receiver
-func (v Vertex) Area() int {
-	return v.X * v.Y
-}
-
-// Scale func : pointer receiver
-func (v *Vertex) Scale(i int) {
-	v.X = v.X * i
-	v.Y = v.Y * i
-}
-
-// Vertex3D struct
-type Vertex3D struct {
-	Vertex
-	Z int
-}
-
-// Area3D func : value receiver
-func (v Vertex3D) Area3D() int {
-	return v.X * v.Y * v.Z
-}
-
-// Scale3D func : pointer receiver
-func (v *Vertex3D) Scale3D(i int) {
-	v.X = v.X * i
-	v.Y = v.Y * i
-	v.Z = v.Z * i
-}
-
-// New : constructor
-func New(x, y, z int) *Vertex3D {
-	return &Vertex3D{Vertex{x, y}, z}
+// Double func
+func (i MyInt) Double() int {
+	return int(i * 2)
 }
 
 func main() {
-	v := New(3, 4, 5)
-	v.Scale(10)
-	fmt.Println(v.Area())
-	fmt.Println(v.Area3D())
+	myInt := MyInt(10)
+	fmt.Println(myInt.Double())
 }
