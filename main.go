74f8935236c9f16445fd354cd087e86b920370f5
Ken Minami <kenminami.dev@gmail.com>
2020-08-06 08:09:13 +0900

    interface

--- main.go
+++ main.go
@@ -2,15 +2,31 @@ package main
 
 import "fmt"
 
-// MyInt : struct
-type MyInt int
+// Human : struct
+type Human interface {
+	Say() string
+}
 
-// Double func
-func (i MyInt) Double() int {
-	return int(i * 2)
+type Person struct {
+	Name string
+}
+
+func (p *Person) Say() string {
+	p.Name = "Mr." + p.Name
+	fmt.Println(p.Name)
+	return p.Name
+}
+
+func DriveCar(human Human) {
+	if human.Say() == "Mr.Mike" {
+		fmt.Println("Run")
+	} else {
+		fmt.Println("Out")
+	}
 }
 
 func main() {
-	myInt := MyInt(10)
-	fmt.Println(myInt.Double())
+	var mike Human = &Person{"Mike"}
+	//mike.Say()
+	DriveCar(mike)
 }
