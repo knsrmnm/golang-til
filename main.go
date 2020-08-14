2f2e5b0025f23170c65b06fc47a83bd9a3920f3c
Ken Minami <kenminami.dev@gmail.com>
2020-08-14 22:31:10 +0900

    sync.Mutex

--- main.go
+++ main.go
@@ -2,22 +2,42 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+type Counter struct {
+	v   map[string]int
+	mux sync.Mutex
+}
+
+func (c *Counter) Inc(key string) {
+	c.mux.Lock()
+	c.v[key]++
+	c.mux.Unlock()
+}
+
+func (c *Counter) Value(key string) int {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	return c.v[key]
+}
+
 func main() {
-	tick := time.Tick(100 * time.Millisecond)
-	boom := time.After(500 * time.Millisecond)
-	for {
-		select {
-		case <-tick:
-			fmt.Println("tick.")
-		case <-boom:
-			fmt.Println("BOOM!")
-			return
-		default:
-			fmt.Println("    .")
-			time.Sleep(50 * time.Millisecond)
+	//c := make(map[string]int)
+	c := Counter{v: make(map[string]int)}
+	go func() {
+		for i := 0; i < 10; i++ {
+			//c["key"] += 1
+			c.Inc("Key")
 		}
-	}
+	}()
+	go func() {
+		for i := 0; i < 10; i++ {
+			//c["key"] += 1
+			c.Inc("Key")
+		}
+	}()
+	time.Sleep(1 * time.Second)
+	fmt.Println(c, c.Value("Key"))
 }
