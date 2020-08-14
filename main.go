b2c8cec73a66d580e450fd180369385de1f42c08
Ken Minami <kenminami.dev@gmail.com>
2020-08-14 21:45:56 +0900

    change buffered channels

--- main.go
+++ main.go
@@ -2,16 +2,20 @@ package main
 
 import "fmt"
 
+func goroutine(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+		c <- sum
+	}
+	close(c)
+}
+
 func main() {
-	ch := make(chan int, 2)
-	ch <- 100
-	fmt.Println(len(ch))
-	ch <- 200
-	fmt.Println(len(ch))
-
-	close(ch)
-
-	for c := range ch {
-		fmt.Println(c)
+	s := []int{1, 2, 3, 4, 5}
+	c := make(chan int, len(s))
+	go goroutine(s, c)
+	for i := range c {
+		fmt.Println(i)
 	}
 }
