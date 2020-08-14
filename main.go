f847b9550f9ec35f7a2a5886fbeb459442783339
Ken Minami <kenminami.dev@gmail.com>
2020-08-14 22:06:05 +0900

    fan-in/fan-out

--- main.go
+++ main.go
@@ -1,34 +1,39 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+import "fmt"
 
-func producer(ch chan int, i int) {
-	// do something
-	ch <- i * 2
+func producer(first chan int) {
+	defer close(first)
+	for i := 0; i < 10; i++ {
+		first <- i
+	}
 }
 
-func consumer(ch chan int, wg *sync.WaitGroup) {
-	for i := range ch {
-		fmt.Println("process: ", i*1000)
-		wg.Done()
+func multi2(first <-chan int, second chan<- int) {
+	defer close(second)
+	for i := range first {
+		second <- i * 2
+	}
+}
+
+func multi4(second chan int, third chan int) {
+	defer close(third)
+	for i := range second {
+		third <- i * 4
 	}
 }
 
 func main() {
-	var wg sync.WaitGroup
-	ch := make(chan int)
 
-	// Producer
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go producer(ch, i)
+	first := make(chan int)
+	second := make(chan int)
+	third := make(chan int)
+
+	go producer(first)
+	go multi2(first, second)
+	go multi4(second, third)
+
+	for result := range third {
+		fmt.Println(result)
 	}
-
-	// Consumer
-	go consumer(ch, &wg)
-	wg.Wait()
-	close(ch)
 }
