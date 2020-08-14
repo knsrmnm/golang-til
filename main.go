76250d371cf0f21c7194f162048459e440cc4f06
Ken Minami <kenminami.dev@gmail.com>
2020-08-14 22:22:43 +0900

    default selection

--- main.go
+++ main.go
@@ -5,32 +5,19 @@ import (
 	"time"
 )
 
-func goroutine1(ch chan string) {
-	for {
-		ch <- "packet from 1"
-		time.Sleep(1 * time.Second)
-	}
-}
-
-func goroutine2(ch chan string) {
-	for {
-		ch <- "packet from 2"
-		time.Sleep(1 * time.Second)
-	}
-}
-
 func main() {
-	c1 := make(chan string)
-	c2 := make(chan string)
-	go goroutine1(c1)
-	go goroutine2(c2)
-
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
 	for {
 		select {
-		case msg1 := <-c1:
-			fmt.Println(msg1)
-		case msg2 := <-c2:
-			fmt.Println(msg2)
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
 		}
 	}
 }
