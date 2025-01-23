0c5c71228430830568319cf092e80a14151196c5
Ken Minami <kenminami.dev@gmail.com>
2025-01-23 23:01:27 +0900

    Update main.go

--- main.go
+++ main.go
@@ -2,42 +2,15 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"os/user"
 	"time"
 )
 
-type Counter struct {
-	v   map[string]int
-	mux sync.Mutex
-}
-
-func (c *Counter) Inc(key string) {
-	c.mux.Lock()
-	c.v[key]++
-	c.mux.Unlock()
-}
-
-func (c *Counter) Value(key string) int {
-	c.mux.Lock()
-	defer c.mux.Unlock()
-	return c.v[key]
+func init() {
+	fmt.Println("Init")
 }
 
 func main() {
-	//c := make(map[string]int)
-	c := Counter{v: make(map[string]int)}
-	go func() {
-		for i := 0; i < 10; i++ {
-			//c["key"] += 1
-			c.Inc("Key")
-		}
-	}()
-	go func() {
-		for i := 0; i < 10; i++ {
-			//c["key"] += 1
-			c.Inc("Key")
-		}
-	}()
-	time.Sleep(1 * time.Second)
-	fmt.Println(c, c.Value("Key"))
+	fmt.Println("Hello world", time.Now())
+	fmt.Println(user.Current())
 }
